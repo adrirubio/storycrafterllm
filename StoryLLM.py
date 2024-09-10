1cc2a5a6f83130778c30ddc48961744952e0199c
Adrian Rubio-Punal <adrian.rubio.punal@gmail.com>
2024-09-10 18:55:03 +0200

    Auto commit on mar 10 sep 2024 18:55:03 CEST

--- StoryLLM.py
+++ StoryLLM.py
@@ -7,7 +7,13 @@ import matplotlib.pyplot as plt
 import numpy as np
 from datetime import datetime
 
-# load the bookcorpus dataset
+# load the BookCorpus dataset
 dataset = load_dataset("bookcorpus")
 
+# Split the dataset into train and test sets
+train_dataset = dataset["train"].train_test_split(test_size=0.1)["train"]
+test_dataset = dataset["train"].train_test_split(test_size=0.1)["test"]
 
+# Print the size of the train and test sets
+print(f"Train size: {len(train_dataset)}")
+print(f"Test size: {len(test_dataset)}")
