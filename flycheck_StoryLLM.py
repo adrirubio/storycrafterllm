97bd7753fa38eac297504fdd01c56ec1ac7c7a85
Adrian Rubio-Punal <adrian.rubio.punal@gmail.com>
2024-09-10 19:20:03 +0200

    Auto commit on mar 10 sep 2024 19:20:03 CEST

---
+++ flycheck_StoryLLM.py
@@ -0,0 +1,31 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from transformers import GPT2Tokenizer
+from datasets import load_dataset
+import matplotlib.pyplot as plt
+import numpy as np
+from datetime import datetime
+
+# load the BookCorpus dataset
+dataset = load_dataset("bookcorpus")
+
+# Split the dataset into train and test sets
+train_dataset = dataset["train"].train_test_split(test_size=0.1)["train"]
+test_dataset = dataset["train"].train_test_split(test_size=0.1)["test"]
+
+# Print the size of the train and test sets
+print(f"Train size: {len(train_dataset)}")
+print(f"Test size: {len(test_dataset)}")
+
+# Load the GPT-2 tokenizer
+tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
+tokenizer.pad_token = tokenizer.eos_token
+
+# Define the tokenization function
+def tokenize_batch(batch):
+    tokenized_output = tokenizer(
+        batch["text"],
+        padding="")
+
+
