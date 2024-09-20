bf8a6c42eadd66da96b78ab9e0f24a9c929ab4b8
Adrian Rubio-Punal <adrian.rubio.punal@gmail.com>
2024-09-20 20:25:03 +0200

    Auto commit on vie 20 sep 2024 20:25:03 CEST

--- StoryLLM.py
+++ StoryLLM.py
@@ -145,12 +145,12 @@ class FeedForward(nn.Module):
 class Block(nn.Module):
     """ Transformer block: communication followed by computation """
 
-    def __init__(self, n_embd, n_head):
+    def __init__(self, n_embd, n_head, dropout=0.1):
         # n_embed: embedding dimension, n_head: the number of heads we'd like
         super().__init__()
         head_size = n_embd // n_head
-        self.sa = MultiHeadAttention(n_head, head_size)
-        self.ffwd = FeedFoward(n_embd)
+        self.sa = MultiHeadAttention(n_head, head_size, n_embd, dropout)
+        self.ffwd = FeedForward(n_embd, dropout)
         self.ln1 = nn.LayerNorm(n_embd)
         self.ln2 = nn.LayerNorm(n_embd)
 
